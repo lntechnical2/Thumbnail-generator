952775968290442ccdcc36c86161ed4c0aad46f3
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-05-09 16:41:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pytube import YouTube
 
 CHANNEL = os.environ.get("CHANNEL","")
 TOKEN = os.environ.get("TOKEN","")
-API_ID  = int(os.environ.get("API_ID",12345))
+API_ID =int(os.environ.get("API_ID",12345))
 API_HASH = os.environ.get("API_HASH","")
 
 
